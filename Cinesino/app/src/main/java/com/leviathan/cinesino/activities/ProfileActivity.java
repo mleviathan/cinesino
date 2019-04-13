0c80c742a0f84e912cf73400dfdc66ccfbc1f8f8
mleviathan <47294592+mleviathan@users.noreply.github.com>
2019-04-13 15:57:23 +0200

    Initial commit

---
+++ Cinesino/app/src/main/java/com/leviathan/cinesino/activities/ProfileActivity.java
@@ -0,0 +1,10 @@
+package com.leviathan.cinesino.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class ProfileActivity {
+}
