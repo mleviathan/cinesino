7d3e8e8d298d7d87e9f74b8638de192ed8e66f7f
leviathanchan <lawrencecafagna@gmail.com>
2019-04-23 17:06:22 +0200

    More fragments

--- Cinesino/app/src/main/java/com/leviathan/cinesino/MainActivity.java
+++ Cinesino/app/src/main/java/com/leviathan/cinesino/MainActivity.java
@@ -3,10 +3,17 @@ package com.leviathan.cinesino;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager _fragManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -15,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    Fragment mapFrag = _fragManager.findFragmentById(R.id.mapFragment);
+                    //TODO Show map fragment
                     return true;
                 case R.id.navigation_dashboard:
                     return true;
@@ -32,6 +41,17 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        _fragManager = getSupportFragmentManager();
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
 
 }
